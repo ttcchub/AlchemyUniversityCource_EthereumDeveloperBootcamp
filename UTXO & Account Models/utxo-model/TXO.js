a6397cca28d222f8c4fac10c0bda9677f8619d17
ttcchub <79540594+ttcchub@users.noreply.github.com>
2023-09-11 22:52:58 +0300

    Add files via upload

---
+++ UTXO & Account Models/utxo-model/TXO.js
@@ -0,0 +1,7 @@
+Transaction Outputs
+Now it's time to create an object for Transaction Outputs(TXOs)!
+
+Using a Bitcoin Block Explorer you can look up TXOs on the actual network. If we wanted to look up UTXOs for a particular address, for instance:
+
+https://blockchain.info/unspent?active=1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa
+
